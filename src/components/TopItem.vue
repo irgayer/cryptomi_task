f0360bcaa7d7f329276f3fe6c6e0d9e8541cb1ed
Yerbol <yerbol.irgaliyev@gmail.com>
2023-12-14 00:43:41 +0600

    no frontend abilities

--- src/components/TopItem.vue
+++ src/components/TopItem.vue
@@ -6,17 +6,56 @@ export default {
 </script>
 
 <template>
-    <div class="css-wd8hou">
+    <!-- <div class="css-wd8hou">
         <p>#{{ top }}</p>
         <p> {{ pair.symbol }}</p>
         <img src="../assets/ethereum.webp">
-    </div>
+    </div> -->
+    <a class="chakra-link css-hcs0of">
+        <div class="css-trdxez">
+            <p class="chakra-text css-0" style="color: {{ (top <= 3) ? yellow : black }}">#{{ top }}</p>
+            <p class="chakra-text css-10apc93">{{ pair.symbol }}</p><img alt="{{ pair.symbol }}" class="chakra-image css-1vi1txg"
+                src="../assets/ethereum.webp">
+        </div>
+    </a>
 </template>
 
 <style scoped>
-.css-wd8hou {
+a {
+    background-color: transparent;
+    color: inherit;
+    text-decoration: inherit;
+}
+.css-hcs0of {
+    transition-property: var(--chakra-transition-property-common);
+    transition-duration: var(--chakra-transition-duration-fast);
+    transition-timing-function: var(--chakra-transition-easing-ease-out);
+    cursor: pointer;
+    text-decoration: none;
+    outline: transparent solid 2px;
+    outline-offset: 2px;
+    color: inherit;
+    font-size: var(--chakra-fontSizes-sm);
+    display: inline-block;
+    min-width: max-content;
+}
+
+.css-trdxez {
     display: flex;
     -webkit-box-align: center;
     align-items: center;
+    gap: var(--chakra-space-1);
+    color: var(--chakra-colors-yellow-400);
+}
+
+.css-10apc93 {
+    white-space: nowrap;
+    text-transform: uppercase;
+}
+
+.css-1vi1txg {
+    width: 20px;
+    height: 20px;
+    border-radius: 100%;
 }
 </style>
--- src/components/TopList.vue
+++ src/components/TopList.vue
@@ -16,9 +16,68 @@ export default {
 </script>
 
 <template>
-    <div class="">
-        <a v-for="pair in pairs">
-            <TopItem v-bind:pair="pair" :top=++top></TopItem>
-        </a>
-    </div>
+
+        <div class="css-k2osh3"></div>
+        <div class="css-10id09q">
+            <p class="chakra-text css-ax34ea">Trending</p>
+        </div>
+        <div class="css-xamd77">
+            <div class="css-qm29w9">
+                <a v-for="pair in pairs">
+                    <TopItem v-bind:pair="pair" :top=++top></TopItem>
+                </a>
+            </div>
+        </div>
+
 </template>
+
+<style scoped>
+.css-qm29w9 {
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    flex-wrap: nowrap;
+    gap: var(--chakra-space-4);
+    width: 100%;
+}
+
+.css-xamd77 {
+    overflow-x: auto;
+    margin-right: var(--chakra-space-8);
+    flex: 1 1 0%;
+}
+
+.css-10id09q {
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    font-weight: 600;
+    gap: var(--chakra-space-1);
+}
+
+.css-ax34ea {
+    display: flex;
+}
+
+.css-k2osh3 {
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    flex: 1 1 0%;
+    padding-top: var(--chakra-space-3);
+    padding-bottom: var(--chakra-space-3);
+}
+
+.css-137m7ew {
+    display: flex;
+    flex-direction: column;
+    padding-inline-start: var(--chakra-space-2);
+    padding-inline-end: var(--chakra-space-2);
+    background: rgb(251, 252, 253);
+    font-size: var(--chakra-fontSizes-xs);
+    position: static;
+    top: 56px;
+    z-index: 998;
+    height: 48px;
+}
+</style>
